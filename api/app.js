efe9db119b4c92c6374c4232ad6ea55548b982df
Spine <igniteboost.net@gmail.com>
2025-02-11 15:28:57 -0500

    Another serverless attempt

--- api/app.js
+++ api/app.js
@@ -8,21 +8,16 @@ import * as user_service from '../src_api/userService.js';
 import * as game_service from '../src_api/gameService.js';
 import * as helper_service from '../src_api/helper.js';
 import mongo from '../models/mongo.js';
-import schedule from 'node-schedule';
+import cors from 'cors';
 
-
-import cors from 'cors'
 const app = express();
-const port = 4000;
-// Mounting the app under the process.env.ROUTE as a parent route (if present)
-let route = process.env.ROUTE;
-let parent_app;
 
 // Enable CORS for all routes (dev)
 app.use(cors());
 
+// Connect to MongoDB
 mongo.connectToServer(function (err, client) {
-  if (err) console.log(err)
+  if (err) console.log(err);
 });
 
 // Configure cookie-session middleware
@@ -37,45 +32,35 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
-// body parser set up
-app.use(bodyParser.urlencoded({ 
-  limit: '200mb',
-  extended: true
-}));
-app.use(bodyParser.json({
-  limit: '200mb',
-  extended: true
-}));
+// Body parser setup
+app.use(
+  bodyParser.urlencoded({
+    limit: '200mb',
+    extended: true,
+  })
+);
+app.use(
+  bodyParser.json({
+    limit: '200mb',
+    extended: true,
+  })
+);
 
-
-// Endpoints for FE
+// Define routes for the API
 app.post('/api/signup', user_service.signup);
 app.post('/api/login', user_service.login);
 app.post('/api/sheet', game_service.updatePoints);
-
 app.get('/api/wrestler', game_service.getWrestler);
 app.get('/api/wrestler/all', game_service.getWrestlers);
 app.get('/api/team', game_service.getTeam);
 app.get('/api/team/all', game_service.getAllTeams);
 app.get('/api/authorize', helper_service.getAccessToken);
 app.get('/api/user', user_service.getUser);
-
 app.put('/api/draft', game_service.draftWrestler);
 app.put('/api/drop', game_service.dropWrestler);
 app.put('/api/team', game_service.editTeam);
 
-//var notify_job = schedule.scheduleJob('*/2 * * * *', () => helper_service.getAccessToken()) //'15 12 * * 1-5'
-//var notify_job = schedule.scheduleJob('*/5 * * * *', () => game_service.updatePoints()) 
-
-// Optional fallthrough error handler
-app.use(function onError(err, req, res, next) {
-  // The error id is attached to `res.sentry` to be returned
-  // and optionally displayed to the user for support.
-  res.statusCode = 500;
-  res.end(res.sentry + "\n");
-});
-
-// Export the app as a handler function for Vercel
+// Export the Express app as a handler function for Vercel
 export default (req, res) => {
-  app(req, res);
+  app(req, res); // This passes requests to Express
 };
